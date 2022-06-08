d18f5b294d304472b6f27232960e75f3713a322c
Amstrong <monachelloa@uninorte.edu.co>
2022-06-07 22:04:09 -0500

    Initial Commit

---
+++ Finances/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Finances
+//
+//  Created by Amstrong Monachello on 7/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Finances/FinancesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancesApp.swift
+//  Finances
+//
+//  Created by Amstrong Monachello on 7/06/22.
+//
+
+import SwiftUI
+
+@main
+struct FinancesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
